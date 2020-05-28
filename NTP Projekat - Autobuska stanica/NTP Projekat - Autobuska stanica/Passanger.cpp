19acb385e1ec1b07493200f65575a8dc4001dc70
adi618 <59510889+adi618@users.noreply.github.com>
2020-05-28 16:57:05 +0200

    update

---
+++ NTP Projekat - Autobuska stanica/NTP Projekat - Autobuska stanica/Passanger.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+#include "Structs.h"
+
+void driver()
+{
+	// ucitaj datume
+
+	int option;
+	int index;
+	int num;
+
+	while (true)
+	{
+		system("cls");
+		std::cout << "\n\n\t    ";
+
+		std::cout << "\n\n\tChoose an option:"
+			<< "\n\t\t1 - ispisi redovne voznje"
+			<< "\n\t\t2 - Kupi kartu"
+			<< "\n\t\t3 - Isprintaj kartu"
+			<< "\n\t\t4 - "
+			<< "\n\n\t\tOption: ";
+
+		std::cin >> option;
+
+		if (option == 1)
+		{
+
+		}
+		else if (option == 2)
+		{
+
+		}
+		else if (option == 3)
+		{
+
+		}
+		else if (option == 10)
+			return;
+		else
+		{
+			std::cout << "\n\t\tInvalid option!";
+		}
+		std::cout << "\n\n\t\t";
+		system("pause");
+	}
+}
