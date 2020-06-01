bba107110bf4aaa2aea86a32ab2a377bb3c39b3e
adi618 <59510889+adi618@users.noreply.github.com>
2020-06-01 21:00:29 +0200

    update

--- NTP Projekat - Autobuska stanica/NTP Projekat - Autobuska stanica/Owner.cpp
+++ NTP Projekat - Autobuska stanica/NTP Projekat - Autobuska stanica/Owner.cpp
@@ -1,48 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 #include "Structs.h"
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-void loadDriver(std::vector <Drivers>& vec)
-=======
+
 void pause();
 
 void loadDriver(std::vector <Driver>& vec)
->>>>>>> Stashed changes
 {
 
 	while (true)
 	{
 		std::cout << "";
 	}
-};
+}
+
 void deleteDriver(std::vector <Driver>& vec)
 {
 
-};
+}
+
 void printDrivers(const std::vector <Driver>& vec)
 {
 
-};
+}
+
 void printBusses(const std::vector <LocalBus>& vec)
 {
 
-};
+}
 
->>>>>>> Stashed changes
 void owner()
 {
 	// login
-<<<<<<< Updated upstream
-	// ja sam mislio da se vlasnik log inuje samo pomocu passworda, buduci da ima samo jedan vlasnik
-	// password vlasnika isto nek bude enkriptiran, kasnije mozemo napravit funkciju za enkriptiranje
-	// i taj enkriptirani broj/string nek bude u nekom txt fileu
-	
-	
-=======
+
 	long passwordOwnerInput;
 	long passwordOwnerTrue;
 	int numberOfTries = 3;
@@ -78,7 +70,6 @@ void owner()
 		}
 	}
 	ownerPassword.close();
->>>>>>> Stashed changes
 
 	int option;
 	int index;
@@ -86,16 +77,7 @@ void owner()
 
 	while (true)
 	{
-<<<<<<< Updated upstream
-		system("cls");
-		std::cout << "\n\n\t    ";
 
-		std::cout << "\n\n\tChoose an option:"
-			<< "\n\t\t1 - registruj vozaca"		// registrovanje nek snimi vozace u vozaci.txt: ime prezime ID busID password
-			<< "\n\t\t2 - otpusti vozaca"
-			<< "\n\t\t3 - ispisi sve informacije vozaca"
-			<< "\n\t\t4 - ispisi sve informacije autobusa"
-=======
 		system("CLS");
 		std::cout << "\n\n\tDobrodosli!"
 			<< "\n\n\tOdaberite opciju:"
@@ -103,7 +85,6 @@ void owner()
 			<< "\n\t\t2 - Otpusti vozaca"
 			<< "\n\t\t3 - Ispisi sve informacije o vozacima"
 			<< "\n\t\t4 - Ispisi sve informacije o autobusima"
->>>>>>> Stashed changes
 			<< "\n\n\t\tOption: ";
 
 		std::cin >> option;
@@ -126,14 +107,9 @@ void owner()
 		}
 		else
 		{
-<<<<<<< Updated upstream
-			std::cout << "\n\t\tInvalid option!";
-		}
-=======
 			std::cout << "\n\t\tPogresan unos!";
 		}
 
->>>>>>> Stashed changes
 		std::cout << "\n\n\t\t";
 		system("pause");
 	}
